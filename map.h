7748bdad7fe1bc440ef51553ed1f7942f3fd5bae
Conflictus <arslanbek.bekbauov@gmail.com>
2023-03-07 20:46:14 +0600

    com

---
+++ map.h
@@ -0,0 +1,17 @@
+#pragma onse
+#include <SFML/Graphics.hpp>
+using namespace sf;
+const int WINDOW_WIDTH = 1260;
+const int WINDOW_HEIGHT = 720;
+const int WIDTH_MAP = 21;
+const int HEIGHT_MAP = 12;
+class Map {
+public:
+    Map();
+    void draw(RenderWindow &window);
+private: 
+    Image mapImage;
+    Texture mapTexture;
+    Sprite mapSprite;
+    void init();
+};
